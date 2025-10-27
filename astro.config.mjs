37267e9690db0b584adf97d5d6841f899c99d642
Mauricio Ojeda <mauricioojeda@MacBook-Air-de-Mauricio.local>
2025-10-27 18:35:20 -0300

    001

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://agenciasur-spa.github.io",
+  base: "/helpdesk/",
+  trailingSlash: "always",
+});
